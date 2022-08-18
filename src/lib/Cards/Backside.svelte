677c3f065846dd074ed71ad936f50df8886a7996
Valerie <valerieknape@gmail.com>
2022-08-18 20:34:16 +0200

    Back and front design of cards added

---
+++ src/lib/Cards/Backside.svelte
@@ -0,0 +1,13 @@
+<div class="back"></div>
+
+
+<style>
+    .back{
+    height: 180px;
+    width:160px;
+    border-radius: 10px;
+    background:linear-gradient(135deg, #5b247a 0%,#1bcedf 100%);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);    }
+    
+</style>
+
