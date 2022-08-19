8d57bba3887c9fceb42929e71ecf91a2de43e04d
Valerie <valerieknape@gmail.com>
2022-08-19 10:06:02 +0200

    Added card grid, modified app body

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Button from '$lib/Button.svelte';
+	import Backside from '$lib/Cards/Backside.svelte';
+
+	type Cards = { type: Number; open: Boolean };
+	let cards: Cards[] = [];
+
+	for (let i = 0; i < 32; i++) {
+		cards.push({ type: i % 16, open: false });
+	}
+</script>
+
+<div class="wrap">
+	<div class="grid">
+		{#each cards as card}
+			<Backside />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(8, 1fr);
+		gap: 10px;
+		width: 1200px;
+	}
+	.wrap {
+		display: flex;
+		align-items: flex-end;
+		justify-content: center;
+		height: 100vh;
+		margin-bottom: 2%;
+	}
+</style>
